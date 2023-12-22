53a9a5687703989719b24810b23d7cec6c2d915c
prasadmantri9 <prasadmantri9@gmail.com>
2023-12-22 17:43:05 +0530

    First Commit

---
+++ src/main/java/PrasadMantri/TestingProject/App.java
@@ -0,0 +1,7 @@
+package PrasadMantri.TestingProject;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/PrasadMantri/TestingProject/FirstTest.java
@@ -0,0 +1,15 @@
+package PrasadMantri.TestingProject;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void FirstTest() {
+	    assertTrue(true);
+	    System.out.println("Lali");
+	  }
+
+}
