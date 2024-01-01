1e53aafab52b78093c41f982f54d71455bc6e478
prasadmantri9 <prasadmantri9@gmail.com>
2024-01-01 18:38:18 +0530

    develp branch learning

--- src/test/java/PrasadMantri/TestingProject/AppTest.java
+++ src/test/java/PrasadMantri/TestingProject/AppTest.java
@@ -9,5 +9,24 @@ public class AppTest {
   @Test
   public void shouldAnswerWithTrue() {
     assertTrue(true);
+    System.out.println("Lali");
+    System.out.println("Branching Part 1");
+    System.out.println("Branching Git Part 2");
+    
+    // Asian
+    System.out.println("Branching Git Part 3");
+    System.out.println("Branching Git Part 4");
+  }
+  
+  @Test
+  public void shouldAnswerWithTrue1() {
+    assertTrue(true);
+    System.out.println("Lali");
+    System.out.println("Branching Part 1");
+    System.out.println("Branching Git Part 2");
+    
+    // Asian
+    System.out.println("Branching Git Part 3");
+    System.out.println("Branching Git Part 4");
   }
 }
